9d5093751e268db075b40c1d75fb12de689ab4d8
Daquaris Chadwick <sourhead5@gmail.com>
2018-06-27 01:03:23 -0400

    Fixed ping check and added hostname lookup algorithm (Sorta Untested)

--- network_devices.py
+++ network_devices.py
@@ -4,17 +4,30 @@ import os, subprocess, ipaddress
 class NetworkList:
     def __init__(self):
         self.devices = []
+        self.deviceNames = []
     def discoverDevices(self, echo_requests = 1):
         subnet = ipaddress.ip_network(u'192.168.1.0/23', strict=False)
         for x in subnet.hosts():
             IP = str(x)
-            output = subprocess.call(["ping", '-n', str(echo_requests), '-i', '1', IP])
+            if '192.168.0' in IP:
+                continue
+            output = subprocess.call(["ping", '-n', str(echo_requests), '-w', '1', IP])
             if output == 0:
                 self.devices.append(IP)
-                print(IP + " alive")
-            print(IP + " checked" + str(output))
+                print(IP + " alive")                
             # TODO resolve device names
-            
+        for x in self.devices:
+            output = subprocess.Popen(["ping", '-n', str(echo_requests), '-a', '-w', '1', IP], stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+            if '[' not in output:
+                continue
+            newString = ""
+            charArray = enumerate(output)
+            for y in range(8, 23):
+                if charArray[y] == '[':
+                    break;
+                newString += charArray[y]
+            if newString != "":
+                self.deviceNames.append(newString)
     # iterate through all hostnames in the subnet mask
     def list_all_devices(self):
         retString = 'The current connected device '
